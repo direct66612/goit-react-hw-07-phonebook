fc164ae92fd80e2f1e07a8b1cbbdcde3fc232bd5
direct66612 <113045624+direct66612@users.noreply.github.com>
2023-12-25 18:26:05 -0400

    update

--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -1,14 +1,14 @@
-import { useSelector } from 'react-redux';
-import { useDispatch } from 'react-redux';
-import { deleteContact } from '../../Redux/contactsSlice';
-import Container from './List.styled';
-import { selectorContacts } from '../../Redux/selectors';
-import { selectorFilter } from '../../Redux/selectors';
+import { useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
+import { deleteContact } from "../../Redux/contactsSlice";
+import Container from "./List.styled";
+import { selectorContacts } from "../../Redux/selectors";
+import { selectorFilter } from "../../Redux/selectors";
 const List = () => {
   const contacts = useSelector(selectorContacts);
   const filter = useSelector(selectorFilter);
   const getContacts = (contacts, filter) => {
-    return contacts.filter(contact =>
+    return contacts.filter((contact) =>
       contact.name.toLowerCase().includes(filter.toLowerCase())
     );
   };
